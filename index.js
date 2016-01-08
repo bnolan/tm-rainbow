da542ec98945da67f6692a9107d59386c426d7a2
Ben Nolan <bnolan@gmail.com>
2016-01-08 14:04:48 +1300

    Added gradual mode for ansi-256 terminals.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var chalk = require('chalk');
+var tinygradient = require('tinygradient')
+var colors = require('ansi-256-colors')
 
-module.exports = function (text, palette) {
+function simple (text, palette) {
   var colors = palette.split(' ').map(function (color) { return chalk[color] });
   var index = 0;
 
@@ -9,4 +11,38 @@ module.exports = function (text, palette) {
   }
 
   return text.split('').map(rainbow).join('')
+}
+
+function gradual (text, palette, length) {
+	var gradient = tinygradient(palette.split(' ')).rgb(length)
+
+	function ansiColor(c) {
+		return colors.fg.getRgb(parseInt(c._r / 43), parseInt(c._g / 43), parseInt(c._b / 43));
+	}
+
+	var result = '';
+	var start = 0;
+	var i = start;
+
+	for (j=0;j<text.length;j++) {
+		result += ansiColor(gradient[i % length]) + text[j];
+
+		i++;
+
+		if (text[j] == '\n'){
+			start++;
+			i = start;
+		}
+	}
+
+	return result;
+}
+
+module.exports = function (text, palette, cycleLength) {
+
+  if (!cycleLength) {
+  	return simple(text, palette);
+  } else {
+  	return gradual(text, palette, cycleLength);
+  }
 }
---
+++ test.js
@@ -0,0 +1,4 @@
+var rainbow = require('./index')
+
+console.log(rainbow('Sup bro! No worries, this kiwi as kumara is as chronic.\nAs a good as pukeko. Mean while, in behind the.\nBicycle shed, Manus Morissette and Rhys Darby were up to no.\nGood with a bunch of random keas. The hard case force of his playing\nrugby was on par with Mrs Falanis mean as jersey. More\nDrugs, more threesomes, giz a hoon, happy as larry.', 'red green blue', 16))
+
